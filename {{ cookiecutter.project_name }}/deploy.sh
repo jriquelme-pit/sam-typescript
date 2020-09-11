6bd9fa6da8bfe7cd515172aadbadd600d1d174c2
joshe <jriquelme.info@gmail.com>
2020-09-11 18:45:50 -0300

    rename

--- {{ cookiecutter.project_name }}/deploy.sh
+++ {{ cookiecutter.project_name }}/deploy.sh
@@ -23,22 +23,6 @@ echo ${FILENAME}
 
 aws s3 cp openapi.yaml ${BUCKET} --sse
 
-echo '...cleaning distribution folder'
-rm -rf dist && mkdir dist && mkdir dist/layers && mkdir dist/tmp
-
-echo '...building production libraries node_modules'
-cd ./libs && rm -rf node_modules && npm install && tsc && cd ..
-
-echo '...packaging layer'
-cd ./src && claudia pack --force --output ../dist/tmp/layer.zip >> /dev/null && cd ..
-unzip dist/tmp/layer.zip 'node_modules/*' -d dist/layers/nodejs/ >> /dev/null && rm -rf dist/tmp
-cd ./dist/layers && zip -r9 ../layers.zip nodejs >> /dev/null && cd ../.. &&
-
-echo '...building production source code node_modules'
-cd ./src && rm -rf node_modules && npm install --production >> /dev/null && cd ..
-cd ./src && tsc && cd ..
-
-sam package --output-template-file packaged.yaml --template-file cloudformation.yaml --s3-bucket ${S3_BUCKET} \
-&& sam deploy --template-file packaged.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME}
+sh ./build.sh
 
 exit 0
